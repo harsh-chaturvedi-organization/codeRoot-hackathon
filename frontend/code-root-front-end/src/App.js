fb01b7710ea0d3c38e05e4c56a831fb1547d23e4
ssss <dethesharad13@gmail.com>
2021-11-19 23:42:06 +0530

    files added

--- frontend/code-root-front-end/src/App.js
+++ frontend/code-root-front-end/src/App.js
@@ -3,16 +3,18 @@ import HomePage from "./Components/HomePage";
 import {UserPage} from "./Components/Login";
 import {Route,Routes} from "react-router-dom"
 import VenderProfile from "./Components/VenderProfile";
-
+import UserHome from "./Components/UserHome";
+import {Nav} from "./Components/Nav"
 function App() {
   return (
     <div>
-      <Routes>
+     {/* <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<UserPage/>} />
-      </Routes>
+     </Routes>*/}
       {/* <VenderProfile/> */}
-
+     <Nav/>
+     <UserHome/>
     </div>
   );
 }
--- frontend/code-root-front-end/src/Components/Nav.jsx
+++ frontend/code-root-front-end/src/Components/Nav.jsx
@@ -1,40 +1,29 @@
+import "./Nav.css"
 import { useState } from "react";
 import GoogleLogin from "react-google-login";
 import VenderProfile from "./VenderProfile";
-export const Navbar = () => {
+
+export function Nav(){
     const [userData, setUser] = useState({});
     const [isLogin, setLogin] = useState(false);
     const [visible, setVis] = useState(false)
-
-    
     const responseGoogle = async (response) => {
         const data = response.profileObj;
         console.log(data);
         setUser({...data});
         setLogin(true);
-        // axios({
-        //   method: "post",
-        //   mode: "no-cors",
-        //   url: "http://localhost:2345/user/login",
-        //   data: data,
-        // })
-        //   .then(function (response) {
-        //     setUser(data);
-        //     setLogin(true);
-        //     // console.log(user);
-        //   })
-        //   .catch(function (error) {
-        //     console.log(error);
-        //   });
-    };
-    return (
-        <div>
+    }
+    return <div className="navBlock">
+    <img className="logo" src="https://ph-files.imgix.net/7aadc0e4-ea0e-479a-a01c-d8a1da7213fc.png?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=80&h=80&fill-color=fff&fit=fill&fill=solid&dpr=1" alt="logo"></img>
+    <input className="inp" type="text"></input>
+
+    <div>
     {/*<input type="text" />*/}
             {isLogin ? (
                 <div>
             {/*<img onMouseOver={() => setVis(true)} src={userData.imageUrl} alt="name" />*/}
                     <div onMouseLeave={() => setVis(false)} style={{ display: visible ? "block" : "block" }}>
-                        <VenderProfile  img={userData.imageUrl}/>
+                        <img className="profileimg" src={userData.imageUrl} alt=""/>
                     </div>
                 </div>
             ) : (
@@ -49,5 +38,5 @@ export const Navbar = () => {
                 </div>
             )}
         </div>
-    );
-};
+    </div>
+}
