ac8cfdbbf2e41c8c6e1d3dbf8f0e52a47791db5d
kkm980 <kantkrishna751@gmail.com>
2021-11-19 22:08:33 +0530

    adding product modal added

--- frontend/code-root-front-end/src/App.js
+++ frontend/code-root-front-end/src/App.js
@@ -3,14 +3,20 @@ import HomePage from "./Components/HomePage";
 import {UserPage} from "./Components/Login";
 import {Route,Routes} from "react-router-dom"
 import VenderProfile from "./Components/VenderProfile";
+import AddProdModal from "./Components/AddProdModal";
 
 function App() {
   return (
     <div>
+    
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<UserPage/>} />
       </Routes>
+    
+      {
+        // <AddProdModal/>
+      }
       {/* <VenderProfile/> */}
 
     </div>
