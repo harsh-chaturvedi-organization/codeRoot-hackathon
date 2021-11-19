01af54f9626d8a69b48915a359c8fed628569dcb
kkm980 <kantkrishna751@gmail.com>
2021-11-19 18:14:01 +0530

    user modal and controller added

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router();
+
+const User = require("../model/user.model")
+
+
+router.post("/login", async (req, res) => {
+    try {
+        console.log(req.body.email)
+        let user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            user = await User.create({
+                email: req.body.email,
+                imageUrl: req.body.imageUrl,
+                googleId: req.body.googleId,
+                name: req.body.name,
+                
+            })
+        }
+        res.status(200).send(user);
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(401).send(err.message)
+    }
+})
+
+
+module.exports = router
